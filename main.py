f258d65a7552d621eeaec58db5f976f19298d14f
Kami <josephkiarie561@gmail.com>
2025-10-09 21:53:52 +0300

    new blockiiiiiiiiiies

--- main.py
+++ main.py
@@ -248,6 +248,11 @@ def main(window):
     # floor
     block_size = 96
     floor = [Block(i * block_size, HEIGHT - block_size, block_size) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
+    objects = [
+        *floor,
+        Block(0, HEIGHT - block_size * 2, block_size),
+        Block(block_size * 3, HEIGHT - block_size * 4, block_size)
+    ]
 
     offset_x = 0
     scroll_area_width = 200
@@ -267,10 +272,10 @@ def main(window):
         
         # players movement
         player.loop(FPS)
-        handle_movement(player, floor)
+        handle_movement(player, objects)
 
         # draw stuff
-        draw(window, background, bg_image, player, floor, offset_x)
+        draw(window, background, bg_image, player, objects, offset_x)
 
         # moving beyond screen edge
         if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or (
