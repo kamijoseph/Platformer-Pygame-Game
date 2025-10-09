f790fcaebd35f7cfcdb5febe69017910bf6306d0
Kami <josephkiarie561@gmail.com>
2025-10-09 14:38:41 +0300

    object class

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ class Player(pygame.sprite.Sprite):
     ANIMATION_DELAY = 3
 
     def __init__(self, x, y, width, height):
+        super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0
@@ -116,6 +117,31 @@ class Player(pygame.sprite.Sprite):
     
     def draw(self, win):
         win.blit(self.sprite, (self.rect.x, self.rect.y))
+
+# objects class
+class Objects(pygame.sprite.Sprite):
+    
+    def __init__(self, x, y, width, height, name):
+        super().__init__()
+
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
+
+    def draw(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+# block class
+class Block(Objects):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = load_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+
+    
 
 # get background function
 def get_background(name):
